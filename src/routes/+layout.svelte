0ab1d1a766a7d0713a12bcb8821c743a39c8525c
KyteKode <231786375+KyteKode@users.noreply.github.com>
2026-03-17 21:06:24 -0400

    Added description and more styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,7 @@
 </script>
 
 <svelte:head>
+	<title>ZipShield</title>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,21 +3,27 @@
     <img src={slogan} alt="Stop the snap." id="slogan" />
 </div>
 
-<p>
-    Isn't it a bit annoying when you're running
-    and your zipper keeps flailing around, slapping you?
-</p>
+<div id="problem">
+    <p>
+        Isn't it annoying when you're running
+       and your zipper keeps flailing around, slapping you?
+    </p>
+    <p class="big_text">
+        ZipShield has a solution for you!
+    </p>
+</div>
 
-<p class="big_text">
-    ZipShield has a solution for you!
-</p>
-
-<div>
+<div id="zs_pictures">
     <img src={placeholder_img} alt="Placeholder" class="zs_picture"/>
     <img src={placeholder_img} alt="Placeholder" class="zs_picture"/>
     <img src={placeholder_img} alt="Placeholder" class="zs_picture"/>
     <img src={placeholder_img} alt="Placeholder" class="zs_picture"/>
 </div>
+
+<p>
+    ZipShield solves this problem by attaching to the zipper's pull tab and the inside of your jacket.
+    This product is 3d printed with PLA filament.
+</p>
 
 
 
@@ -25,50 +31,101 @@
     import title from "$lib/assets/title.png";
     import slogan from "$lib/assets/slogan.png";
 
-    const placeholder_img = "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png";
+    const placeholder_img = "https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=";
 </script>
 
 
 
 <style lang="scss">
-    @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100..700;1,100..700&display=swap');
+    @import url("https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100..700;1,100..700&display=swap");
 
     :root {
-        background-color: #041826;
+        background: linear-gradient(30deg, #1474b9, #062338);
+        //background-color: #093453;
         color: #ffffff;
 
         font-family: "IBM Plex Sans", sans-serif;
         font-size: 1.35rem;
 
         font-weight: bold;
+        overflow-x: hidden;
+    }
+
+    #problem {
+        //background-color: #00689c;
+        background-color: #b3e1ff;
+        color: #003d66;
+
+        box-sizing: border-box;
+        width: 100%;
+
+        padding: 3rem;
+        margin: auto;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
     }
 
     p {
         text-align: center;
         margin: 0.5rem;
+        font-size: 1.35rem;
     }
 
     .big_text {
-        font-size: 2rem;
-        margin: 1rem;
+        font-size: 3rem;
+        margin: 2rem;
     }
 
-    .zs_picture {
-        max-width: 30vw;
-        max-height: 30vw;
+    #zs_pictures {
+        display: flex;
+        align-items: center;
+        justify-content: center;
 
-        width: 20rem;
-        height: 20rem;
+        width: 100%;
+        margin: auto;
 
-        margin: 2rem;
+        margin-bottom: 3rem;
 
-        @supports (corner-shape: squircle) {
-            border-radius: 40% / 40%;
-            corner-shape: squircle;
+        &:has(img:hover) img {
+            transform: scale(0.85);
         }
 
-        @supports not (corner-shape: squircle) {
-            border-radius: 20% / 20%;
+        img:hover {
+            transform: scale(1.2);
+            box-shadow: 0 0 5px #00aaff, 0 0 40px #38bdff, 0 0 80px #89d8ff;
+        }
+
+        img {
+            max-width: 22.5vw;
+            max-height: 22.5vw;
+
+            width: 20rem;
+            height: 20rem;
+
+            margin: auto;
+            margin-top: 3rem;
+
+            border: 7px solid #062339;
+
+            transition: transform 0.3s cubic-bezier(0.37, 0, 0.63, 1),
+                box-shadow 0.3s cubic-bezier(0.37, 0, 0.63, 1);
+
+            &:hover {
+                transform: scale(1.2) !important;
+                box-shadow: 0 0 5px #00aaff, 0 0 40px #38bdff, 0 0 80px #89d8ff;
+            }
+
+            @supports (corner-shape: squircle) {
+                border-radius: 40% / 40%;
+                corner-shape: squircle;
+            }
+
+            @supports not (corner-shape: squircle) {
+                border-radius: 20% / 20%;
+            }
         }
     }
 
